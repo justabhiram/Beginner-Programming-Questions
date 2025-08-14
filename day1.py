ec980f6d15f8aaae8bf045ed62b75823bcf9b95d
Abhiram Mamidi <abhiramsdesk@gmail.com>
2025-08-15 03:54:31 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,21 @@
+# 1. Print "Namah Shivaya!"
+print("Namah Shivaya!")
+
+# 2. Taking two numbers as input and print their sum
+a = 5
+b = 10
+print(a + b)
+
+# 3. Take a name as input and print "Namah Shivaya, [name]!"
+name = "Prabhas"
+print("Namah Shivaya", name, "!")
+
+# 4. Calculate the area of a rectangle (given length and width)
+l = 7
+b = 3
+print("The area of the rectangle is: ", l*b)
+
+# 5. Convert Celsius to Fahrenheit
+C = int(input("Enter temperature in °C:"))
+F = ((9/5)*C) + 32
+print("Temperature in °F is:", F)
